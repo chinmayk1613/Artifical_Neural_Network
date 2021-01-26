ec0740dab03d74d6d0b97b85e32bbb8cf3d6dee5
kchinmay <cskck77@gmail.com>
2021-01-26 20:38:01 +0100

    ANN
    Artificial Neural Network

---
+++ Bank_Customer_Retention.py
@@ -0,0 +1,83 @@
+#import library
+
+import numpy as np   #contain maths
+import tensorflow as tf
+import pandas as pd #to import dataset and to manage data set
+
+
+
+#####Data Pre-Processing######
+
+#Importing Dataset#
+dataset=pd.read_csv('Churn_Modelling.csv') #load data set
+X=dataset.iloc[:,3:-1].values    #independnt variables
+y=dataset.iloc[:, -1].values     #dependent data**************MISTAKE
+
+#Encoding Categorical Data#
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+labelencoder_X=LabelEncoder()
+X[:,2]=labelencoder_X.fit_transform(X[:,2])
+#here problem is that machine learning algo thinks that 0<2 meaning
+# France is less than spain but this is not the case at all
+#hence we use dummy column buidling three column
+#meanig put 1 if that France is there for ex. and put 0 if not.
+ct=ColumnTransformer(transformers=[('encoder',OneHotEncoder(),[1])],remainder='passthrough')
+X=np.array(ct.fit_transform(X))
+
+
+#Split Data in train and test set#
+from sklearn.model_selection import train_test_split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0)
+
+#Feature Scalling#It is very very important and compulsory when doing Deep Learning
+from sklearn.preprocessing import StandardScaler
+sc_X=StandardScaler()
+X_train=sc_X.fit_transform(X_train)
+X_test=sc_X.transform(X_test)
+
+#####Building the ANN######
+
+#Initializing the ANN#
+ann = tf.keras.models.Sequential() #Intializes ANN as a sequence of layers
+
+#Adding input and first hidden layer#
+ann.add(tf.keras.layers.Dense(units=6,activation='relu'))
+
+#Adding second hidden layer#
+ann.add(tf.keras.layers.Dense(units=6,activation='relu'))
+
+#Adding the output Layer#
+ann.add(tf.keras.layers.Dense(units=1,activation='sigmoid'))
+#when two or more output, actication fucntion would be #softmax
+
+
+#####Training the ANN#####
+
+#Compiling the ANN#
+ann.compile(optimizer= 'adam',loss= 'binary_crossentropy',metrics=['accuracy'])
+# when more than two output use #categorical_crossentropy
+
+#Training the ANN on training set
+ann.fit(X_train,y_train,batch_size=32,epochs=100)
+
+#####Making the Predictions and evaluating the model#####
+
+#Predicting the result of a single observation
+print(ann.predict(sc_X.transform([[1,0,0,600,1,40,3,60000,2,1,1,50000]]))>0.5)
+#nned to input info in 2d double bracket array
+#replace the categorical value as dummy value which map to one hot encoding values
+#then predict this values on scalled value varible using transform method cause our maodel is trained on scalled values during
+  #feature scalling
+
+
+#####Predicting The Test Result#####
+y_pred=ann.predict(X_test)
+y_pred=(y_pred > 0.5)# True->1(0.5 to 1)  False->0(0 to 0.5)
+#print(np.concatenate((y_pred.reshape(len(y_pred),1),y_test.reshape(len(y_test),1)),1))
+
+#####Confusion Metrics#####
+from sklearn.metrics import confusion_matrix, accuracy_score
+cm=confusion_matrix(y_test,y_pred)
+print(cm)
+print(accuracy_score(y_test,y_pred))
